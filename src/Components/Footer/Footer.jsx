c16f924bfc41f67455658275f9417c5063617453
Sahil <sahilk64555@gmail.com>
2024-08-05 22:56:30 +0530

    added all the files

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import './Footer.css'
-import InstaIcon from '../../assets/InstaIcon.svg'
-import MozillaIcon from '../../assets/MozzilaIcon.png'
-import LinkedlnIcon from '../../assets/LinkedlnIcon.svg'
-import GithubIcon from '../../assets/GithubIcon.svg'
-import TwitterIcon from '../../assets/TwitterIcon.svg'
+import React from 'react';
+import './Footer.css';
+import igIcon from '../../assets/Instagram1.png';
+import MozIcon from '../../assets/Mozilla1.png';
+import LinkIcon from '../../assets/Linkedln1.png';
+import GitIcon from '../../assets/Github1.png';
+import XIcon from '../../assets/Twitter1.png';
 
 const Footer = () => {
-return (
-    <div className='footer'>
-        <p>© 2024 Codezilla SRMIST. All rights reserved</p>
-        <ul>
-            {/* Need to fix the width of all the icons to align */}
-        <li><a href="https://www.instagram.com/codezillaclub/" target="_blank"><img src={InstaIcon} alt="" /></a></li>
-        <li><a href="https://github.com/CodezillaClub" target="_blank"><img src={GithubIcon} alt="" /></a></li>
-        <li><a href="https://community.mozilla.org/de/groups/codezilla/" target="_blank"><img src={MozillaIcon} alt="" /></a></li>
-        <li><a href="https://www.linkedin.com/company/codezillaclub/" target="_blank"><img src={LinkedlnIcon} alt="" /></a></li>
-        <li><a href="https://x.com/CodezillaClub" target="_blank"><img src={TwitterIcon} alt="" /></a></li>
-        </ul>
-    </div>
-)
-}
+    return (
+        <div className='footer'>
+            <p>© 2024 Codezilla SRMIST. All rights reserved</p>
+            <ul>
+                <li>
+                    <a href="https://www.instagram.com/codezillaclub/" target="_blank" rel="noopener noreferrer">
+                        <img src={igIcon} alt="Instagram" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/CodezillaClub" target="_blank" rel="noopener noreferrer">
+                        <img src={GitIcon} alt="GitHub" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://community.mozilla.org/de/groups/codezilla/" target="_blank" rel="noopener noreferrer">
+                        <img src={MozIcon} alt="Mozilla" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/company/codezillaclub/" target="_blank" rel="noopener noreferrer">
+                        <img src={LinkIcon} alt="LinkedIn" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://x.com/CodezillaClub" target="_blank" rel="noopener noreferrer">
+                        <img src={XIcon} alt="Twitter" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
