c15b5326876d26a0b43f83f9f1c9b05d0a632f74
Sahil <sahilk64555@gmail.com>
2024-08-25 15:45:49 +0530

    Updated the Hero BG

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -8,7 +8,7 @@ return (
         <div className='hero-text'>
             <h1>CODEZILLA || MOZILLA COMMUNITY || OPEN SOURCE || SRM IST CLUB</h1>
             <p>Codezilla is a Mozilla Campus Community in SRMIST, Ramapuram to provide the Youth, in and around the campus, an open source platform, where they can develop their technical knowledge and skill set for a better career. We organize technical events and hands-on sessions to scale up their experties. It provides a good environment to engage the students in different activities and develop their projects.</p>
-            <a href='http://codezilla-one.vercel.app' target='__onblank'><button className='btn'><b>Register for Open Source Odyssey</b><img src={arrow} alt="" /></button></a>
+            <a href='https://community.mozilla.org/en/groups/codezilla/' target='__onblank'><button className='btn'><b>Discover more about us on the Mozilla Community Portal</b><img src={arrow} alt="" /></button></a>
         </div>
     </div>
 )
