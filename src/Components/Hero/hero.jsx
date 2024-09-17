c2aa3eb8160562c089b29b977220008085a97dd3
Sahil <sahilk64555@gmail.com>
2024-09-17 15:53:19 +0530

    updated recruitment form

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -8,7 +8,7 @@ return (
         <div className='hero-text'>
             <h1>CODEZILLA || MOZILLA COMMUNITY || OPEN SOURCE || SRM IST CLUB</h1>
             <p>Codezilla is a Mozilla Campus Community in SRMIST, Ramapuram to provide the Youth, in and around the campus, an open source platform, where they can develop their technical knowledge and skill set for a better career. We organize technical events and hands-on sessions to scale up their experties. It provides a good environment to engage the students in different activities and develop their projects.</p>
-            <a href='https://community.mozilla.org/en/groups/codezilla/' target='__onblank'><button className='btn'><b>Discover more about us on the Mozilla Community Portal</b><img src={arrow} alt="" /></button></a>
+            <a href='https://docs.google.com/forms/d/e/1FAIpQLSf6Ouj7T7bSMg8J6KzKGbGnvtFR13kIX11o5icUUOJG3nfhWw/viewform' target='__onblank'><button className='btn'><b>Unlock your potential with Codezilla,    apply today!</b><img src={arrow} alt="" /></button></a>
         </div>
     </div>
 )
